a0226a3d0e3a19b76487223bb16baeaf18aaefde
Mai Quoc Viet <duvimuahe123@gmail.com>
2024-11-25 12:40:17 +0700

    update file

--- script.js
+++ script.js
@@ -1,18 +1,69 @@
-// Snowflake generator
-function createSnowflake() {
-    const snowflake = document.createElement('div');
-    snowflake.className = 'snowflake';
-    snowflake.style.left = `${Math.random() * 100}vw`;
-    snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`;
-    snowflake.style.opacity = Math.random();
-    snowflake.style.fontSize = `${Math.random() * 10 + 10}px`;
-    snowflake.innerText = '❄';
-    document.body.appendChild(snowflake);
-  
-    setTimeout(() => {
-      snowflake.remove();
-    }, 5000);
-  }
-  
-  setInterval(createSnowflake, 50);
-  
+const $ = (id) => {
+    return document.getElementById(id);
+};
+
+const $days = $('days');
+const $hours = $('hours');
+const $minutes = $('minutes');
+const $seconds = $('seconds');
+const targetDate = new Date('2024-12-24T23:59');
+
+const updateTimer = () => {
+    let totalInSec = Math.floor(
+        (targetDate.valueOf() - new Date().valueOf()) / 1000
+    );
+
+    if (totalInSec < 0) {
+        totalInSec = 0;
+    }
+
+    const seconds = Math.floor(
+        totalInSec % 60
+    );
+    const minutes = Math.floor(
+        (totalInSec / 60) % 60
+    );
+    const hours = Math.floor(
+        (totalInSec / 60 / 60) % 24
+    );
+    const days = Math.floor(
+        totalInSec / 60 / 60 / 24
+    );
+    const format = (num) => {
+        return `${num}`.padStart(2, '0');
+    };
+
+    $seconds.innerText =
+        format(seconds);
+    $minutes.innerText =
+        format(minutes);
+    $hours.innerText = format(hours);
+    $days.innerText = format(days);
+};
+setInterval(updateTimer, 1000);
+
+const createSnowFlake = () => {
+    const $snow = document.createElement('i');
+    $snow
+        .classList
+        .add('fas', 'fa-snowflake', 'snowflake');
+
+    $snow.style.left =
+        `${Math.random() * window.innerWidth}px`;
+    $snow.style.animationDuration =
+        `${(Math.random() * 3 + 2)}s`;
+    $snow.style.opacity =
+        Math.random();
+    $snow.style.fontSize =
+        `${Math.random() * 10}px`;
+    $snow.style.filter = 'blur(1px)'
+
+    document.body.appendChild($snow);
+    $snow.onanimationend = () => {
+        $snow.remove();
+    };
+
+    requestAnimationFrame(createSnowFlake);
+};
+
+requestAnimationFrame(createSnowFlake);
